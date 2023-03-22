e52a79cb15fc5f73a716d25404a94106cf02c426
Ruan Borges Lins <ruanblins@gmail.com>
2023-03-22 15:06:13 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Project Screenshot
+
+![Desktop screenshot](./images/dscreenshot.png)
+![Mobile screenshot](./images/mscreenshot.png)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- JavaScript
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,74 @@
+const billInput = document.querySelector(".bill-input input");
+const tipButtons = document.querySelectorAll(".select-tip-percentage");
+const customInput = document.querySelector("#custom")
+const peopleNumber = document.querySelector(".people-input input");
+const personTip = document.querySelector(".tip-person");
+const personTotal = document.querySelector(".total-person");
+const resetBtn = document.querySelector(".reset-btn");
+
+//reset button
+resetBtn.addEventListener('click', () => {
+    billInput.value = ""
+    customInput.value = ""
+    peopleNumber.value = ""
+    personTip.innerText = "$0.00"
+    personTotal.innerText = "$0.00"
+    tipButtons.forEach(button => {
+        button.classList.remove("active")
+        tipButtons[1].classList.add("active")
+    })
+})
+
+
+// add and remove active class on tip buttons
+tipButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        tipButtons.forEach(button => {
+            button.classList.remove("active")
+        })
+        e.target.classList.add("active")
+        if (!customInput.classList.contains("active")) {
+            customInput.value = ""
+        }
+    })
+})
+
+//get the tip values
+const calculateTip = () => {
+    const billValue = parseFloat(billInput.value);
+    const people = parseInt(peopleNumber.value);
+    const customTip = document.querySelector("#custom.active")
+    let tipPercentage = parseInt(document.querySelector(".select-tip-percentage.active").dataset.percentage);
+
+    if (customTip) {
+        tipPercentage = parseFloat(customTip.value)
+    }
+
+    const tipTotal = +(billValue * (tipPercentage / 100)).toFixed(2);
+    const tipPerson = +(tipTotal / people).toFixed(2);
+    const billTotalPerson = +((billValue + tipTotal) / people).toFixed(2);
+
+    personTotal.innerText = `$${billTotalPerson}`
+    personTip.innerText = `$${tipPerson}`
+}
+
+//calculate tip
+billInput.addEventListener("keyup", () => {
+    if (billInput.value != "" && billInput.value > 1 && peopleNumber.value > 0 && peopleNumber.value != "") {
+        calculateTip();
+    }
+})
+
+peopleNumber.addEventListener("keyup", () => {
+    if (billInput.value != "" && billInput.value > 1 && peopleNumber.value > 0 && peopleNumber.value != "") {
+        calculateTip();
+    }
+})
+
+
+
+
+
+
+
+
